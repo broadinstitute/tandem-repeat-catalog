564a16a7b5ffe4f6b796f73efe379e89965bedf2
bw2 <ben.weisburd@gmail.com>
2024-07-28 12:03:44 -0400

    Added 'trgt validate' to check catalog format. Refactored paths

--- all_steps.merge_and_annotate_loci.sh
+++ all_steps.merge_and_annotate_loci.sh
@@ -1,15 +1,38 @@
 set -ex
 
-# install the latest version of the str-analysis package
-#python3 -m pip install --upgrade git+https://github.com/broadinstitute/str-analysis.git
+# install str-analysis python package
+if ! python3 -m pip show str-analysis &> /dev/null
+then
+    python3 -m pip install --upgrade git+https://github.com/broadinstitute/str-analysis.git
+fi
 
-REFERENCE_FASTA_PATH=~/hg38.fa
-GENCODE_GTF_PATH=gencode.v46.basic.annotation.gtf.gz
+OUTPUT_PREFIX=$(realpath repeat_catalog.3x_and_9bp.2_to_1000bp_motifs.hg38)
+
+REFERENCE_FASTA_PATH=$(realpath hg38.fa)
+GENCODE_GTF_PATH=$(realpath gencode.v46.basic.annotation.gtf.gz)
+
+if [ ! -f ${REFERENCE_FASTA_PATH} ]
+then
+    echo ERROR: ${REFERENCE_FASTA_PATH} file not found
+    exit 1
+fi
+
+if [ ! -f ${GENCODE_GTF_PATH} ]
+then
+    echo ERROR: ${GENCODE_GTF_PATH} file not found
+    exit 1
+fi
+
+mkdir -p results
+cd results
+
+
+SECONDS=0
+
 # STEP #1  (already done for hg38, and results are publicly available. Will just download them in step #2)
-
 #git clone git@github.com:broadinstitute/colab-repeat-finder.git
 #cd colab-repeat-finder/python
-#python3 perfect_repeat_finder.py  --min-repeats 3  --min-span 9  --min-motif-size 1  --max-motif-size 50  --output-prefix perfect_repeats.hg38  --show-progress-bar  ${REFERENCE_FASTA_PATH}
+#python3 perfect_repeat_finder.py  --min-repeats 3  --min-span 9  --min-motif-size 1  --max-motif-size 1000  --output-prefix perfect_repeats.hg38  --show-progress-bar  ${REFERENCE_FASTA_PATH}
 
 
 # STEP #2:  generate catalog
@@ -19,10 +42,10 @@ ALL_PERFECT_REPEATS_CATALOG_URL=https://storage.googleapis.com/str-truth-set/hg3
 ILLUMINA_CATALOG_URL=https://storage.googleapis.com/str-truth-set/hg38/ref/other/illumina_variant_catalog.sorted.bed.gz
 TRUTH_SET_CATALOG_URL=https://storage.googleapis.com/str-truth-set-v2/filter_vcf/all_repeats_including_homopolymers_keeping_loci_that_have_overlapping_variants/combined/combined.51_samples.positive_loci.json
 
-wget -nc $KNOWN_DISEASE_ASSOCIATED_LOCI_URL
-wget -nc $ALL_PERFECT_REPEATS_CATALOG_URL
-wget -nc $ILLUMINA_CATALOG_URL
-wget -nc $TRUTH_SET_CATALOG_URL
+wget -qnc $KNOWN_DISEASE_ASSOCIATED_LOCI_URL
+wget -qnc $ALL_PERFECT_REPEATS_CATALOG_URL
+wget -qnc $ILLUMINA_CATALOG_URL
+wget -qnc $TRUTH_SET_CATALOG_URL
 
 KNOWN_DISEASE_ASSOCIATED_LOCI_PATH=$(basename ${KNOWN_DISEASE_ASSOCIATED_LOCI_URL})
 ALL_PERFECT_REPEATS_CATALOG_PATH=$(basename ${ALL_PERFECT_REPEATS_CATALOG_URL})
@@ -106,50 +129,61 @@ python3 -u -m str_analysis.annotate_and_filter_str_catalog --verbose \
 	--max-motif-size 1000 \
 	--min-interval-size-bp 1 \
 	--discard-overlapping-intervals-with-similar-motifs \
-	--output-path repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz \
+	--output-path ${OUTPUT_PREFIX}.merged_and_annotated.json.gz \
 	repeat_catalog.3x_and_9bp.hg38.merged.json.gz
 
 
-# STEP #3: convert the catalog from ExpansionHunter catalog format to TRGT, LongTR, HipSTR, and GangSTR formats
-python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_trgt_catalog --split-adjacent-repeats repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz  --output-file repeat_catalog.3x_and_9bp.hg38.TRGT.bed &
-python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_longtr_format  repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz  --output-file repeat_catalog.3x_and_9bp.hg38.LongTR.bed &
-python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_hipstr_format  repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz  --output-file repeat_catalog.3x_and_9bp.hg38.HipSTR.bed &
-python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_gangstr_spec   repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz  --output-file repeat_catalog.3x_and_9bp.hg38.GangSTR.bed &
+# STEP #3: convert the catalog from ExpansionHunter catalog format to bed, TRGT, LongTR, HipSTR, and GangSTR formats
+python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_bed --split-adjacent-repeats ${OUTPUT_PREFIX}.merged_and_annotated.json.gz  --output-file ${OUTPUT_PREFIX}.bed.gz &
+python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_trgt_catalog --split-adjacent-repeats ${OUTPUT_PREFIX}.merged_and_annotated.json.gz  --output-file ${OUTPUT_PREFIX}.TRGT.bed &
+python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_longtr_format  ${OUTPUT_PREFIX}.merged_and_annotated.json.gz  --output-file ${OUTPUT_PREFIX}.LongTR.bed &
+python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_hipstr_format  ${OUTPUT_PREFIX}.merged_and_annotated.json.gz  --output-file ${OUTPUT_PREFIX}.HipSTR.bed &
+python3 -m str_analysis.convert_expansion_hunter_variant_catalog_to_gangstr_spec   ${OUTPUT_PREFIX}.merged_and_annotated.json.gz  --output-file ${OUTPUT_PREFIX}.GangSTR.bed &
 wait
 
+# Confirm that the TRGT catalog passes 'trgt validate'
+trgt validate --genome ${REFERENCE_FASTA_PATH}  --repeats ${OUTPUT_PREFIX}.TRGT.bed
 
+# Spot-check several known disease-associated loci to make sure they made it into the final catalog
+for end_coord   in  3074933  69037304  39348479  41746032  80147139;    # HTT, FXN, RFC1, PHOX2B, EIF4A3
+do
+    grep $'\t'${end_coord}$'\t'  ${OUTPUT_PREFIX}.TRGT.bed
+done
 
 
 # STEP #4: create release_draft folder and compute catalog stats
 timestamp=$(date '+%Y-%m-%d')
 release_draft_folder=release_draft_${timestamp}
 mkdir -p ${release_draft_folder}
-for path in repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.bed.gz \
-	repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.bed.gz.tbi \
-	repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz \
-	repeat_catalog.3x_and_9bp.hg38.TRGT.bed.gz \
-	repeat_catalog.3x_and_9bp.hg38.LongTR.bed \
-	repeat_catalog.3x_and_9bp.hg38.HipSTR.bed \
-	repeat_catalog.3x_and_9bp.hg38.GangSTR.bed;
+for path in ${OUTPUT_PREFIX}.bed.gz \
+	${OUTPUT_PREFIX}.bed.gz.tbi \
+	${OUTPUT_PREFIX}.merged_and_annotated.json.gz \
+	${OUTPUT_PREFIX}.TRGT.bed \
+	${OUTPUT_PREFIX}.LongTR.bed \
+	${OUTPUT_PREFIX}.HipSTR.bed \
+	${OUTPUT_PREFIX}.GangSTR.bed;
 do
     cp ${path} ${release_draft_folder}/
 done
 
-python3 -m str_analysis.compute_catalog_stats --verbose repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz
+python3 -m str_analysis.compute_catalog_stats --verbose ${OUTPUT_PREFIX}.merged_and_annotated.json.gz
 
 
 # STEP #5: compare it to other catalogs
-wget -nc https://s3.amazonaws.com/gangstr/hg38/genomewide/hg38_ver17.bed.gz
-wget -nc https://storage.googleapis.com/str-truth-set/hg38/ref/other/adotto_tr_catalog_v1.2.bed.gz
-wget -nc https://storage.googleapis.com/str-truth-set/hg38/ref/other/mukamel_VNTR_catalog.bed.gz
-wget -nc https://storage.googleapis.com/str-truth-set/hg38/ref/other/vamos_catalog.v2.1.bed.gz
+mkdir -p comparisons
+cd comparisons
+
+wget -qnc https://s3.amazonaws.com/gangstr/hg38/genomewide/hg38_ver17.bed.gz
+wget -qnc https://storage.googleapis.com/str-truth-set/hg38/ref/other/adotto_tr_catalog_v1.2.bed.gz
+wget -qnc https://storage.googleapis.com/str-truth-set/hg38/ref/other/mukamel_VNTR_catalog.bed.gz
+wget -qnc https://storage.googleapis.com/str-truth-set/hg38/ref/other/vamos_catalog.v2.1.bed.gz
 
 python3 -u -m str_analysis.convert_gangstr_spec_to_expansion_hunter_variant_catalog --verbose hg38_ver17.bed.gz
 
 for catalog_filename in mukamel_VNTR_catalog.bed.gz  vamos_catalog.v2.1.bed.gz hg38_ver17.variant_catalog.json    adotto_tr_catalog_v1.2.bed.gz
 do
     echo Processing ${catalog_filename}
-    output_prefix=$(echo ${catalog_filename} | sed 's/.bed.gz$//' | sed 's/.variant_catalog.json$//')
+    current_catalog_output_prefix=$(echo ${catalog_filename} | sed 's/.bed.gz$//' | sed 's/.variant_catalog.json$//')
 
     python3 -m str_analysis.annotate_and_filter_str_catalog \
 	    -r ${REFERENCE_FASTA_PATH} \
@@ -158,15 +192,17 @@ do
 	    --skip-disease-loci-annotations \
 	    --min-motif-size 2 \
 	    --max-motif-size 1000 \
-	    --output-path ${output_prefix}.annotated_and_filtered.json \
+	    --output-path ${current_catalog_output_prefix}.annotated_and_filtered.json \
 	    --verbose \
 	    ${catalog_filename}
 
-    python3 -m str_analysis.compute_catalog_stats --verbose ${output_prefix}.annotated_and_filtered.json
+    python3 -m str_analysis.compute_catalog_stats --verbose ${current_catalog_output_prefix}.annotated_and_filtered.json
 
     python3 -u -m str_analysis.merge_loci --verbose  \
-	    --output-prefix ${output_prefix}  \
+	    --output-prefix ${current_catalog_output_prefix}  \
 	    --write-bed-files-with-new-loci \
-	    repeat_catalog.3x_and_9bp.hg38.merged_and_annotated.json.gz \
-	    ${output_prefix}.annotated_and_filtered.json
+	    $(basename ${OUTPUT_PREFIX}).merged_and_annotated.json.gz \
+	    ${current_catalog_output_prefix}.annotated_and_filtered.json
 done
+
+echo "Done.  Total time: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
