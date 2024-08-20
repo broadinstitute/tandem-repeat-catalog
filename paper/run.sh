f97066550e7424bea5466334a22ba0fde872afd2
bw2 <ben.weisburd@gmail.com>
2024-08-20 12:57:22 -0400

    paper/run.sh

--- paper/run.sh
+++ paper/run.sh
@@ -1,4 +1,7 @@
 set -ex
 
-nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa
-#cat nohup.out | grep -v collaps > all_steps.merge_and_annotate_loci.log
+nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa --force
+cat nohup.out | grep -v collaps > compare_catalogs.log
+
+#nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa
+#cat nohup.out | grep -v collaps > compare_catalogs.stats_only.log
