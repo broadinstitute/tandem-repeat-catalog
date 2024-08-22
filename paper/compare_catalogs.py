fc6af0c5d922c3c5a0bd66fe68d8995bdf85cc6f
bw2 <ben.weisburd@gmail.com>
2024-08-22 08:49:38 -0400

    added UCSC simple repeats track

--- paper/compare_catalogs.py
+++ paper/compare_catalogs.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--hg38-reference-fasta", default="hg38.fa", help="Path of hg38 reference genome FASTA file")
 parser.add_argument("--dry-run", action="store_true", help="Print commands without running them")
 parser.add_argument("--force", action="store_true", help="Run annotation step even if the output files already exist")
-
+parser.add_argument("-k", "--keyword", help="Only process catalogs that contain this keyword")
 args = parser.parse_args()
 
 
@@ -57,11 +57,12 @@ catalogs_in_order = [
 	("GangSTR_v17", "https://s3.amazonaws.com/gangstr/hg38/genomewide/hg38_ver17.bed.gz"),
 	("HipSTR_catalog", "https://github.com/HipSTR-Tool/HipSTR-references/raw/master/human/hg38.hipstr_reference.bed.gz"),
 	("perfect_repeats_in_hg38", "https://storage.googleapis.com/str-truth-set/hg38/ref/other/colab-repeat-finder/hg38_repeats.motifs_1_to_1000bp.repeats_3x_and_spans_9bp/hg38_repeats.motifs_1_to_1000bp.repeats_3x_and_spans_9bp.bed.gz"),
-	("polymorphic_loci_in_HPRC_assemblies", "https://storage.googleapis.com/str-truth-set-v2/filter_vcf/all_repeats_including_homopolymers_keeping_loci_that_have_overlapping_variants/combined/combined.51_samples.positive_loci.json"),
+	("polymorphic_loci_in_HPRC_assemblies", "https://storage.googleapis.com/str-truth-set-v2/filter_vcf/all_repeats_including_homopolymers_keeping_loci_that_have_overlapping_variants/combined/combined.78_samples.positive_loci.json"),
 	("platinumTRs_v1.0", "https://zenodo.org/records/13178746/files/human_GRCh38_no_alt_analysis_set.platinumTRs-v1.0.trgt.bed.gz"),
 	("adotto_TRs_catalog_v1.2", "https://storage.googleapis.com/str-truth-set/hg38/ref/other/adotto_tr_catalog_v1.2.bed.gz"),
 	("mukamel_VNTR_catalog", "https://storage.googleapis.com/str-truth-set/hg38/ref/other/mukamel_VNTR_catalog.bed.gz"),
 	("vamos_catalog_v2.1", "https://storage.googleapis.com/str-truth-set/hg38/ref/other/vamos_catalog.v2.1.bed.gz"),
+	("UCSC_simple_repeat_track", "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/simpleRepeat.txt.gz"),
 ]
 
 
@@ -101,7 +102,8 @@ catalogs_in_order = [("primary_known_disease_associated_loci", primary_disease_a
 catalog_paths["primary_known_disease_associated_loci"] = primary_disease_associated_loci_path
 
 # convert PlatinumTR catalog to ExpansionHunter catalog format for comparison
-if "platinumTRs_v1.0" in catalog_paths:
+if "platinumTRs_v1.0" in catalog_paths and (
+	not args.keyword or args.keyword.lower() in "platinumTRs_v1.0".lower() or args.keyword.lower() in catalog_paths["platinumTRs_v1.0"].lower()):
 	path_after_conversion = catalog_paths["platinumTRs_v1.0"].replace(".bed.gz", ".catalog.json.gz")
 	if not os.path.isfile(path_after_conversion):
 		run(f"python3 -u -m str_analysis.convert_trgt_catalog_to_expansion_hunter_catalog -r {args.hg38_reference_fasta} {catalog_paths['platinumTRs_v1.0']} -o {path_after_conversion}")
@@ -110,7 +112,8 @@ else:
 	print("WARNING: PlatinumTRs catalog not included in list of catalogs")
 
 # convert GangSTR catalog to ExpansionHunter catalog format for comparison
-if "GangSTR_v17" in catalog_paths:
+if "GangSTR_v17" in catalog_paths and (
+	not args.keyword or args.keyword.lower() in "GangSTR_v17".lower() or args.keyword.lower() in catalog_paths["GangSTR_v17"].lower()):
 	path_after_conversion = catalog_paths["GangSTR_v17"].replace(".bed.gz", ".catalog.json.gz")
 	if not os.path.isfile(path_after_conversion):
 		run(f"python3 -u -m str_analysis.convert_gangstr_spec_to_expansion_hunter_variant_catalog --verbose {catalog_paths['GangSTR_v17']} -o {path_after_conversion}")
@@ -119,16 +122,26 @@ else:
 	print("WARNING: GangSTR catalog not included in list of catalogs")
 
 # convert HipSTR catalog to ExpansionHunter catalog format for comparison
-if "HipSTR_catalog" in catalog_paths:
+if "HipSTR_catalog" in catalog_paths and (
+	not args.keyword or args.keyword.lower() in "HipSTR_catalog".lower() or args.keyword.lower() in catalog_paths["HipSTR_catalog"].lower()):
 	path_after_conversion = catalog_paths["HipSTR_catalog"].replace(".bed.gz", ".catalog.bed.gz")
 	if not os.path.isfile(path_after_conversion):
 		run(f"python3 -u {scripts_dir}/convert_hipstr_catalog_to_regular_bed_file.py {catalog_paths['HipSTR_catalog']} -o {path_after_conversion}")
-	catalog_paths["HipSTR_catalog"] = f"{path_after_conversion}"
+	catalog_paths["HipSTR_catalog"] = path_after_conversion
 else:
 	print("WARNING: HipSTR catalog not included in list of catalogs")
 
+# convert UCSC track to BED format
+if "UCSC_simple_repeat_track" in catalog_paths and (
+	not args.keyword or args.keyword.lower() in "UCSC_simple_repeat_track".lower() or args.keyword.lower() in catalog_paths["UCSC_simple_repeat_track"].lower()):
+	path_after_conversion = catalog_paths["UCSC_simple_repeat_track"].replace(".txt.gz", "") + "_track_from_UCSC.bed.gz"
+	if not os.path.isfile(path_after_conversion):
+		run(f"python3 -u {scripts_dir}/convert_ucsc_simple_repeat_track_to_bed.py {catalog_paths['UCSC_simple_repeat_track']} -o {path_after_conversion}")
+	catalog_paths["UCSC_simple_repeat_track"] = path_after_conversion
+
 # convert Chiu et al catalog to ExpansionHunter catalog format for comparison
-if "comprehensive_catalog_from_Chiu_et_al" in catalog_paths:
+if "comprehensive_catalog_from_Chiu_et_al" in catalog_paths and (
+	not args.keyword or args.keyword.lower() in "comprehensive_catalog_from_Chiu_et_al".lower() or args.keyword.lower() in catalog_paths["comprehensive_catalog_from_Chiu_et_al"].lower()):
 	output_path = "hg38.Chiu_et_al.v1.bed.gz"
 	if not os.path.isfile(output_path):
 		# convert the comprehensive catalog from Chiu et al. to a regular bed file (removing the header and converting to 0-basd coords)
@@ -137,9 +150,12 @@ if "comprehensive_catalog_from_Chiu_et_al" in catalog_paths:
 
 all_stats_tsv_paths = {}
 for catalog_name, path in catalog_paths.items():
+	if args.keyword and args.keyword.lower() not in catalog_name.lower() and args.keyword.lower() not in path.lower():
+		continue
+
 	annotated_catalog_path = re.sub("(.json|.bed)(.gz)?$", "", path) + ".annotated.json.gz"
 	if not os.path.isfile(annotated_catalog_path) or args.force:
-		run(f"""python3 -m str_analysis.annotate_and_filter_str_catalog \
+		run(f"""python3 -m str_analysis.annotate_and_filter_str_catalog --verbose \
 			--reference-fasta {args.hg38_reference_fasta} \
 			--skip-gene-annotations \
 			--skip-disease-loci-annotations \
