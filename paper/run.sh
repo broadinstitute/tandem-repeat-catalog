703991c36281d8f793606353145d035b1d2fffb7
bw2 <ben.weisburd@gmail.com>
2024-10-02 17:01:38 -0400

    updated paper/run.sh

--- paper/run.sh
+++ paper/run.sh
@@ -2,11 +2,6 @@ rm nohup.out
 
 set -ex
 
-nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa  --outer-join --force-annotate  
-cat nohup.out  > compare_catalogs.log
+nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa 
+cp nohup.out compare_catalogs.log
 
-#nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa  --outer-join --force-stats
-#cat nohup.out  > compare_catalogs.stats_only.log
-
-#nohup python3 -u compare_catalogs.py --hg38 ~/hg38.fa  --outer-join
-#cat nohup.out  > compare_catalogs.stats_only.log
